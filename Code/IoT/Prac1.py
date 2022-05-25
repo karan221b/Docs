e1c68ea0cb873380c03591c86928421a92904ec0
Karan <32564087+karan221b@users.noreply.github.com>
2022-05-25 16:32:26 +0530

    Update Prac1.py

--- Code/IoT/Prac1.py
+++ Code/IoT/Prac1.py
@@ -1,1 +1,29 @@
+## 2. Timer based buzzer
+from gpio import *
+from time import *
+pinMode(0, INPUT)
+pinMode(0, OUTPUT)
+while True:
+    if digitalRead(0)== HIGH:
+        sleep(5)
+        digitalWrite (1, 1023)
+    else:
+        digitalWrite (1, 0)
+        
+       
+      
+## 3. Sensor based counting device
 
+from gpio import*
+from time import*
+
+count = 0
+
+pinMode(0, INPUT)
+pinMode(0, OUTPUT)
+
+while True:
+  if digitalRead(0) == HIGH:
+    count = count + 1
+    print("Counting " + str(count))
+    sleep(0.3)
